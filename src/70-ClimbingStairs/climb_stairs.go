6faa3cf681ece58fa9aaab0ad447e54bf75fa350
yangguang_chen <yangguang_chen@kingdee.com>
2018-07-19 14:51:47 +0800

    * climb stairs as fibnacci problem

--- src/70-ClimbingStairs/climb_stairs.go
+++ src/70-ClimbingStairs/climb_stairs.go
@@ -1,12 +1,87 @@
-package cs
-
-// ClimbStairs calculate n steps to reach the top
 // You are climbing a stair case. It takes n steps to reach to the top.
 // Each time you can either climb 1 or 2 steps.
 // In how many distinct ways can you climb to the top?
 // Note: Given n will be a positive integer.
 // see more from https://leetcode.com/problems/climbing-stairs/description/
-func ClimbStairs(n int) int {
 
-	return count
+package cs
+
+import (
+	"math"
+)
+
+// ClimbStairsByBruteForce calculate n steps to reach the top by brute force
+func ClimbStairsByBruteForce(n int) int {
+	return bruteForce(0, n)
+}
+
+func bruteForce(i, n int) int {
+	if i > n {
+		return 0
+	}
+	if i == n {
+		return 1
+	}
+	return bruteForce(i+1, n) + bruteForce(i+2, n)
+}
+
+// ClimbStairsByRecurseMemory calculate by Recursion with memorization
+func ClimbStairsByRecurseMemory(n int) int {
+	memo := make([]int, n+1)
+	return recurseMemory(0, n, memo)
+}
+
+func recurseMemory(i, n int, memo []int) int {
+	if i > n {
+		return 0
+	}
+	if i == n {
+		return 1
+	}
+	if memo[i] > 0 {
+		return memo[i]
+	}
+	memo[i] = recurseMemory(i+1, n, memo) + recurseMemory(i+2, n, memo)
+	return memo[i]
+}
+
+// ClimbStairsByDynamicProgram calculate by dynamic programming
+func ClimbStairsByDynamicProgram(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+// ClimbStairsByFibnacciNumber calculate by fibnacci number
+func ClimbStairsByFibnacciNumber(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	first := 1
+	second := 2
+	var third int
+	for i := 3; i <= n; i++ {
+		third = first + second
+		first = second
+		second = third
+	}
+	return second
+}
+
+// ClimbStairsByFibnacciFormula calculate by fibnacci formula
+func ClimbStairsByFibnacciFormula(n int) int {
+	var sqrt, fibn float64
+
+	sqrt = math.Sqrt(5)
+	fibn = math.Pow((float64(1)+sqrt)/2, float64(n+1)) - math.Pow((float64(1)-sqrt)/2, float64(n+1))
+
+	return (int)(fibn / sqrt)
 }
--- src/70-ClimbingStairs/climb_stairs_test.go
+++ src/70-ClimbingStairs/climb_stairs_test.go
@@ -4,9 +4,119 @@ import (
 	"testing"
 )
 
-func TestClimbStairs(t *testing.T) {
-	var n = 6
-	nums := ClimbStairs(n)
+func TestClimbStairsByBruteForce(t *testing.T) {
+	// brute force
+	var n = 2
+	nums := ClimbStairsByBruteForce(n)
+	if nums != 2 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 3
+	nums = ClimbStairsByBruteForce(n)
+	if nums != 3 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 4
+	nums = ClimbStairsByBruteForce(n)
+	if nums != 5 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 5
+	nums = ClimbStairsByBruteForce(n)
+	if nums != 8 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	// recurse memory
+	n = 3
+	nums = ClimbStairsByRecurseMemory(n)
+	if nums != 3 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 4
+	nums = ClimbStairsByRecurseMemory(n)
+	if nums != 5 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 5
+	nums = ClimbStairsByRecurseMemory(n)
+	if nums != 8 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	// dynamoic programming
+	n = 3
+	nums = ClimbStairsByDynamicProgram(n)
+	if nums != 3 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 4
+	nums = ClimbStairsByDynamicProgram(n)
+	if nums != 5 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 5
+	nums = ClimbStairsByDynamicProgram(n)
+	if nums != 8 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	// fibnacci number
+	n = 3
+	nums = ClimbStairsByFibnacciNumber(n)
+	if nums != 3 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 4
+	nums = ClimbStairsByFibnacciNumber(n)
+	if nums != 5 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 5
+	nums = ClimbStairsByFibnacciNumber(n)
+	if nums != 8 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	// fibnacci formula testing
+	n = 3
+	nums = ClimbStairsByFibnacciFormula(n)
+	if nums != 3 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 4
+	nums = ClimbStairsByFibnacciFormula(n)
+	if nums != 5 {
+		t.Error("fail")
+	}
+	t.Log(nums)
+
+	n = 5
+	nums = ClimbStairsByFibnacciFormula(n)
 	if nums != 8 {
 		t.Error("fail")
 	}
